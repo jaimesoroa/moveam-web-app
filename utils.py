07eabaffd97ab6fd11357167ccf52aaaabbaeda4
Jaime Soroa <jaimesoroa@gmail.com>
2023-11-15 13:05:11 +0100

    Updated secrets, and including database connection

--- utils.py
+++ utils.py
@@ -7,10 +7,7 @@ class Utils:
 
     def check_config():
         '''Returns a message to user for missing configuration
-
-        Args:
-            app (Flask): Flask app object
-
+        
         Returns:
             string: Error info
         '''
@@ -18,24 +15,24 @@ class Utils:
         if st.session_state['AUTHENTICATION_MODE'] == '':
             return 'Please specify one of the two authentication modes'
         if st.session_state['AUTHENTICATION_MODE'].lower() == 'serviceprincipal' and st.session_state['TENANT_ID'] == '':
-            return 'Tenant ID is not provided in the config.py file'
+            return 'Tenant ID is not provided'
         elif st.session_state['REPORT_ID'] == '':
-            return 'Report ID is not provided in config.py file'
+            return 'Report ID is not provided'
         elif st.session_state['WORKSPACE_ID'] == '':
-            return 'Workspace ID is not provided in config.py file'
+            return 'Workspace ID is not provided'
         elif st.session_state['CLIENT_ID'] == '':
-            return 'Client ID is not provided in config.py file'
+            return 'Client ID is not provided'
         elif st.session_state['AUTHENTICATION_MODE'].lower() == 'masteruser':
             if st.session_state['POWER_BI_USER'] == '':
-                return 'Master account username is not provided in config.py file'
+                return 'Master account username is not provided'
             elif st.session_state['POWER_BI_PASS'] == '':
-                return 'Master account password is not provided in config.py file'
+                return 'Master account password is not provided'
         elif st.session_state['AUTHENTICATION_MODE'].lower() == 'serviceprincipal':
             if st.session_state['CLIENT_SECRET'] == '':
-                return 'Client secret is not provided in config.py file'
+                return 'Client secret is not provided'
         elif st.session_state['SCOPE_BASE'] == '':
-            return 'Scope base is not provided in the config.py file'
+            return 'Scope base is not provided'
         elif st.session_state['AUTHORITY_URL'] == '':
-            return 'Authority URL is not provided in the config.py file'
+            return 'Authority URL is not provided'
         
         return None
